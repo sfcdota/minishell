0b57e5d37cba9a167a53bc750be2796b1420cb7b
sfcdota <sfcdota@gmail.com>
2020-11-17 21:03:47 +0300

    ha

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include "minishell.h"
+#include "executor/executor.h"
 
-int main()
+
+  #include <stdio.h>
+
+  
+  void print_env(char **envp)
+  {
+	while(*envp)
+	{
+		ft_putstr_fd(*envp, 1);
+		ft_putchar_fd('\n', 1);
+		envp++;
+	}
+  }
+int main(int argc, char **argv, char *envp[])
 {
 	char *kek;
+	*envp = ft_strdup("hui sosi");
+//	free(envp[0]);
+//	envp[0] = NULL;
+	print_env(envp);
+//	printf("%s\n", envp[0]);
 	kek = malloc(MAX_CMD_LENGTH);
 	while(1)
 	{
@@ -10,6 +29,13 @@ int main()
 			exit(-1);
 		if (!ft_strncmp(kek, "exit\n", MAX_CMD_LENGTH))
 			exit(0);
+//		else
+//		{
+//			char *out = malloc(MAX_CMD_LENGTH);
+//			printf("chdir res = %d\n", chdir("./"));
+//			printf("getcwd res = %s\n", getcwd(out, MAX_CMD_LENGTH));
+//			return (0);
+//		}
 		//parsing
 		//execution
 		ft_memset(kek, 0, MAX_CMD_LENGTH);
