6157e011ee3ae1cf52d2b3ae9d84848b53d5b8de
vovvoy <ormonbaev.marsel@gmail.com>
2020-11-20 20:27:45 +0300

    right split for minishell try 01

--- main.c
+++ main.c
@@ -4,141 +4,185 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-<<<<<<< HEAD
-=======
+
+t_arg	*new_arg(char *name, int is_env)
+{
+	t_arg *arg;
+
+	arg = malloc(sizeof(t_arg));
+	arg->name = name;
+	arg->is_env = is_env;
+	return (arg);
+}
+
 t_cmd	*new_cmd(char *name, char *flags, t_list *args, int std_in, int std_out, int is_separated, int is_bin)
 {
 	t_cmd *cmd;
+	t_arg *arg;
 
+	arg = new_arg("hello", 0);
 	cmd = malloc(sizeof(t_cmd));
 	cmd->name = name;
 	cmd->flags = flags;
-	cmd->args = args;
+	cmd->args = ft_lstnew(arg);
 	cmd->std_in = std_in;
 	cmd->std_out = std_out;
 	cmd->is_separated = is_separated;
 	cmd->is_bin = is_bin;
 	return (cmd);
 }
+
 void print_cmds(t_list *cmd_list)
 {
 	t_cmd *cmd;
+	t_arg *arg;
 	while(cmd_list)
 	{
 		cmd = cmd_list->content;
-		printf("printf = %d\n", printf("name = %s\nflags = %s\n", cmd->name, cmd->flags));
+		arg = cmd->args->content;
+		printf("printf = %d\n\n", printf("name = %s\nflags = %s\narg_name = %s\narg_is_env = %d\n", cmd->name, cmd->flags, arg->name, arg->is_env));
 		cmd_list = cmd_list->next;
 	}
 }
-void	add_echo(t_list *cmd)
+
+void	example(t_info *info)
 {
-	t_cmd	*for_short;
-	for_short = (t_cmd *)(cmd->content);
-	// ft_lstadd_back(&cmd, ft_lstnew(new_cmd(ft_strdup("echo"), ft_strdup(""), -1, 1, 0, 0)));
-	printf("name = %s\nflags = %s\n", for_short->name, for_short->flags);
+	t_cmd	*cmd;
+
+	cmd = new_cmd(ft_strdup("echo"), ft_strdup("-n"), NULL, -1, 1, 0, 0);
+	info->cmd = ft_lstnew(cmd);
+	cmd = new_cmd(ft_strdup("sdfsf"), ft_strdup("sadf"), NULL, -1, 1, 0, 0);
+	ft_lstadd_back(&(info->cmd), ft_lstnew(cmd));
+	cmd = new_cmd(ft_strdup("echo"), ft_strdup(""), NULL, -1, 1, 0, 0);
+	ft_lstadd_back(&(info->cmd), ft_lstnew(cmd));
+	print_cmds(info->cmd);
 }
->>>>>>> 2ae4d1e52f950326f1e89130ea53ec880dc0a063
+int			strl(char *str)
+{
+	char	*s;
+
+	if (!str)
+		return (0);
+	s = str;
+	while (*s)
+		s++;
+	return (s - str);
+}
+
+char		*strj(char *s1, char s2)
+{
+	unsigned int	i;
+	char			*out_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = -1;
+	if (!(out_str = malloc(sizeof(char) * (strl(s1) + 2))))
+		return (NULL);
+	while (s1[++i])
+		out_str[i] = s1[i];
+	free(s1);
+	out_str[i++] = s2;
+	out_str[i] = '\0';
+	return (out_str);
+}
+
+int cmd_count(char *cmd)
+{
+	int i;
+	int j;
+	int cnt;
+	char *str;
+	int len;
+
+	len = strl(cmd);
+	cnt = 0;
+	str = malloc(sizeof(char) * 1);
+	*str = '\0';
+	i = -1;
+
+	while (cmd[++i])
+	{
+		if (len >= i + 1)
+		{
+			if ((cmd[i] == ' ' || cmd[i] == 34 || cmd[i] == 39) && cmd[i + 1] == 39)
+			{
+				i += 2;
+				while (cmd[i] != 39 && i < len)
+				{
+					str = strj(str, cmd[i++]);
+				}
+				printf("\t\t\t\t|%s|\n", str);
+				free(str);
+				if (cmd[i + 1] == ' ')
+					printf("\t\t\t\t| |\n");
+				str = malloc(sizeof(char) * 1);
+				*str = '\0'; 
+				if (i == len)
+					return (-1);
+				cnt++;
+				i++;
+			}
+			if ((cmd[i] == ' ' || cmd[i] == 34 || cmd[i] == 39) && cmd[i + 1] == 34)
+			{
+				i += 2;
+				while (cmd[i] != 34 && i < len)
+				{
+					str = strj(str, cmd[i++]);
+				}
+				printf("\t\t\t\t|%s|\n", str);
+				free(str);
+				if (cmd[i] == ' ')
+					printf("\t\t\t\t| |\n");
+				str = malloc(sizeof(char) * 1);
+				*str = '\0'; 
+				if (i == len)
+					return (-1);
+				cnt++;
+				i++;
+				printf("%c\n", cmd[i]);
+			}
+			
+			if ((cmd[i] == ' ' || i == 0 || cmd[i] == 39 || cmd[i] == 34) && cmd[i + 1] != ' ' && cmd[i] != 34 && cmd[i] != 39)
+			{
+				if (cmd[i] == ' ') 
+					i++;
+				// printf("%d\n", i);
+				while (cmd[i] != ' ' && cmd[i] && cmd[i] != 34 && cmd[i] != 39)
+					str = strj(str, cmd[i++]);
+				// printf("%d\n", i);
+				printf("\t\t\t\t|%s|\n", str);
+				free(str);
+				if (cmd[i] == ' ')
+					printf("\t\t\t\t| |\n");
+				str = malloc(sizeof(char) * 1);
+				*str = '\0';
+				cnt++;
+				i--;
+			}
+		}
+	}
+	return (cnt);
+}
+
+void	parser(char *command)
+{
+	int cmnd;
+
+	if (!command)
+		return ;
+	printf("count = %d\n" , cmd_count(command));
+
+}
 
 int main(int argc, char **argv, char *envp[])
 {
 	t_info info;
-<<<<<<< HEAD
-	t_list t_cmd1;
-	t_cmd *cmd1;
-	t_list t_arg1;
-	t_list t_arg2;
-	t_arg *arg1;
-	t_arg *arg2;
-	t_list t_cmd2;
-	t_cmd *cmd2;
-	t_list t_arg3;
-	t_list t_arg4;
-	t_arg *arg3;
-	t_arg *arg4;
+	example(&info);
 
-		cmd1->name = "echo";
-		cmd1->flags = "-n";
 
-			arg1->name = "hello";
-			arg1->is_env = 0;
-
-			arg2->name = "world";
-			arg2->is_env = 0;
-
-			t_arg1.content = (void *)arg1;
-			t_arg2.content = (void *)arg2;
-
-		cmd1->args->content = &t_arg1;
-		cmd1->args->next = &t_arg2;
-		cmd1->std_in = 0;
-		cmd1->std_out = 1;
-
-		t_cmd1.content = (void *)cmd1;
-
-	info.cmd = &t_cmd1;
-
-		cmd2->name = "echo";
-		cmd1->flags = "-n";
-
-			arg3->name = "school";
-			arg3->is_env = 0;
-
-			arg4->name = "21";
-			arg4->is_env = 0;
-
-			t_arg3.content = (void *)arg3;
-			t_arg4.content = (void *)arg4;
-
-		cmd2->args->content = &t_arg3;
-		cmd2->args->next = &t_arg4;
-		cmd2->std_in = 0;
-		cmd2->std_out = 1;
-
-		t_cmd2.content = (void *)cmd2;
-
-	info.cmd->next = &t_cmd1;
-	printf("hello\n");
-	// printf("%s\n", ((t_cmd *)(info.cmd->content))->name);
-	// info.cmd = info.cmd->next;
-	// printf("%s\n", ((t_cmd *)(info.cmd->content))->name);
-
+	parser("echo this is the first example     \"hello world\"'ABC'     sljfd");
+	
+	
 	return (0);
 }
-
-
-=======
-	t_cmd	*tmp;
-	tmp = new_cmd(ft_strdup("echo"), ft_strdup("-n"), NULL, -1, 1, 0, 0);
-	info.cmd = ft_lstnew(tmp);
-	print_cmds(info.cmd);
-	tmp = new_cmd(ft_strdup("sdfsf"), ft_strdup("sadf"), NULL, -1, 1, 0, 0);
-	ft_lstadd_back(&(info.cmd), ft_lstnew(tmp));
-	tmp = new_cmd(ft_strdup("echo"), ft_strdup(""), NULL, -1, 1, 0, 0);
-	ft_lstadd_back(&(info.cmd), ft_lstnew(tmp));
-	print_cmds(info.cmd);
-	
-	
-	t_cmd *cmd = malloc(sizeof(t_cmd));
-	cmd->name = ft_strdup("");
-	cmd->flags = ft_strdup("");
-	t_list *new = ft_lstnew(cmd);
-	
-	
-	
-//	else
-//	{
-//		char *cmd = ft_strdup("echo");
-//		char *flags = ft_strdup("");
-//		t_list *temp = ft_lstnew(&new_cmd(cmd, flags, -1, 1, 0, 0));
-//		ft_lstadd_back(&info->cmd, temp);
-//		print_cmds(info->cmd);
-//	}
-	// ft_putendl_fd("hui", 1);
-	// add_echo(info->cmd);
-	// add_echo(info.cmd);
-	// add_echo(info.cmd);
-	// printf("kek2");
-	// print_cmds(info.cmd);
-	return (0);
-}
->>>>>>> 2ae4d1e52f950326f1e89130ea53ec880dc0a063
--- minishell.h
+++ minishell.h
@@ -18,16 +18,12 @@ typedef struct			s_cmd
 {
 	char				*name;
 	char				*flags;
+	int 				is_env;
 	t_list				*args;
 	int					std_in;
 	int					std_out;
-<<<<<<< HEAD
-	// int					is_separated;
-	// int					is_bin;
-=======
 	int					is_separated;
 	int					is_bin;
->>>>>>> 2ae4d1e52f950326f1e89130ea53ec880dc0a063
 }						t_cmd;
 
 typedef struct			s_env
